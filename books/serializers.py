d29b7260d7db28ff4abbf2e754c1da511dadec8a
Ruslan <ruslan10897@mail.ru>
2024-10-13 19:10:39 +0300

    Исправил методы добавления и изменения с одинаковыми авторами и жанром

--- books/serializers.py
+++ books/serializers.py
@@ -12,12 +12,27 @@ class AuthorSerializer(serializers.ModelSerializer):
     book_count = serializers.IntegerField(read_only=True)
     class Meta:
         model = Author
-        fields = ['title', 'book_count']
+        fields = ['id', 'title', 'book_count']
 
 class BookSerializer(serializers.ModelSerializer):
-    authors = AuthorSerializer(many=True)
-    genre = GenreSerializer()
+    authors = AuthorSerializer(read_only=True, many=True)
+    genre = GenreSerializer(read_only=True)
 
     class Meta:
         model = Book
         fields = ['id', 'title', 'image', 'authors', 'genre', 'count']
+
+    def validate(self, attrs):
+        request = self.context.get('request')
+        if request.data.get('authors'):
+            authors = []
+            for author in request.data["authors"]:
+                authors.append(
+                    Author.objects.get_or_create(title=author["title"])[0]
+                )
+            attrs["authors"] = authors
+        if request.data.get('title'):
+            if not request.data['genre'].get('title'):
+                raise serializers.ValidationError({'genre': 'Поле title жанра обязательно'})
+            attrs['genre'] = Genre.objects.get_or_create(title=request.data['genre']['title'])[0]
+        return attrs
