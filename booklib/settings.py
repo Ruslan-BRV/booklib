d010bbabb09a59e7abd8b6b6c9573db6a2c4c878
Ruslan <ruslan10897@mail.ru>
2024-10-15 16:50:00 +0300

    поправил settings

--- booklib/settings.py
+++ booklib/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
